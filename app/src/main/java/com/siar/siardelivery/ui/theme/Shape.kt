5311fdcfcd0fafd0f671f77fa3c8bbd1fc39618d
Pablo Quiroga <jtdoe78@gmail.com>
2024-04-01 09:16:32 -0300

    added new colors and shapes

---
+++ app/src/main/java/com/siar/siardelivery/ui/theme/Shape.kt
@@ -0,0 +1,15 @@
+package com.siar.siardelivery.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+/*****
+ * Project: Siar Delivery
+ * Created by: Pablo Daniel Quiroga
+ *****/
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
