3c8a8dea237168c97dfd37194f043668226b7565
Pablo Quiroga <jtdoe78@gmail.com>
2024-04-04 19:08:17 -0300

    added customView for editText and replace login and signup screen

---
+++ app/src/main/java/com/siar/siardelivery/ui/components/CustomEditText.kt
@@ -0,0 +1,92 @@
+package com.siar.siardelivery.ui.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AcUnit
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.siar.siardelivery.R
+import com.siar.siardelivery.ui.theme.DeliveryTheme
+
+/*****
+ * Project: DemoNavigationCompose
+ * Created by: Pablo Daniel Quiroga
+ *****/
+@Composable
+fun CustomEditText(
+    data: String,
+    label: String? = null,
+    icons: List<ImageVector>? = null,
+    transformation: VisualTransformation = VisualTransformation.None,
+    onDataChange: (String) -> Unit
+) {
+    var transformed by remember { mutableStateOf(false) }
+
+    OutlinedTextField(
+        value = data,
+        onValueChange = {
+            onDataChange(it)
+        },
+        label = {
+            Text(text = label ?: "" )
+        },
+        maxLines = 1,
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 24.dp, start = 16.dp, end = 16.dp),
+        colors = TextFieldDefaults.colors(
+            focusedIndicatorColor = colorResource(id = R.color.orange_400),
+            focusedContainerColor = MaterialTheme.colorScheme.background,
+            unfocusedContainerColor = MaterialTheme.colorScheme.background
+        ),
+        trailingIcon = {
+            icons?.apply {
+                val image = if (!transformed)
+                    icons[0] else icons[1]
+
+                IconButton(
+                    onClick = {
+                        //TODO maybe this can be made as abstract for change function
+                        if (icons.size > 1){ transformed = !transformed }
+                    },
+                    content = {
+                        Icon(imageVector = image, contentDescription = "")
+                    }
+                )
+            }
+        },
+        visualTransformation = if (!transformed) transformation else VisualTransformation.None
+    )
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun CustomEdtPreview(){
+    DeliveryTheme {
+        CustomEditText(
+            data = "pepe",
+            label = "label",
+            icons = listOf( Icons.Filled.AcUnit )
+        ) {}
+    }
+}
--- app/src/main/java/com/siar/siardelivery/ui/onboarding/login/views/LoginContent.kt
+++ app/src/main/java/com/siar/siardelivery/ui/onboarding/login/views/LoginContent.kt
@@ -5,37 +5,25 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.siar.siardelivery.R
+import com.siar.siardelivery.ui.components.CustomEditText
 import com.siar.siardelivery.ui.onboarding.login.LoginViewState
 
 /*****
@@ -138,56 +126,27 @@ fun SectionInputs(
     onMailChange: (String) -> Unit,
     onPassChange: (String) -> Unit,
 ) {
-    var passwordVisibility by remember { mutableStateOf(false) }
+    CustomEditText(
+        data = mail,
+        label = stringResource(id = R.string.edit_lbl_mail),
+        icons = listOf(
+            Icons.Filled.Email
+        )
+    ) {
+        onMailChange(it)
+    }
 
-    OutlinedTextField(
-        value = mail,
-        onValueChange = { onMailChange(it) },
-        label = { Text(text = stringResource(id = R.string.edit_lbl_mail)) },
-        maxLines = 1,
-        singleLine = true,
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(top = 24.dp, start = 16.dp, end = 16.dp),
-        colors = TextFieldDefaults.colors(
-            focusedIndicatorColor = colorResource(id = R.color.orange_400),
-            focusedContainerColor = MaterialTheme.colorScheme.background,
-            unfocusedContainerColor = MaterialTheme.colorScheme.background
+    CustomEditText(
+        data = pass,
+        label = stringResource(id = R.string.edit_lbl_pass),
+        icons = listOf(
+            Icons.Filled.VisibilityOff,
+            Icons.Filled.Visibility
         ),
-        trailingIcon = {
-            Icon(imageVector = Icons.Filled.Email, contentDescription = "email icon")
-        },
-    )
-    OutlinedTextField(
-        value = pass,
-        onValueChange = { onPassChange(it) },
-        label = { Text(text = stringResource(id = R.string.edit_lbl_pass)) },
-        maxLines = 1,
-        singleLine = true,
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 16.dp, vertical = 12.dp),
-        colors = TextFieldDefaults.colors(
-            focusedIndicatorColor = colorResource(id = R.color.orange_400),
-            focusedContainerColor = MaterialTheme.colorScheme.background,
-            unfocusedContainerColor = MaterialTheme.colorScheme.background
-        ),
-        trailingIcon = {
-            val image = if (passwordVisibility)
-                Icons.Filled.VisibilityOff else Icons.Filled.Visibility
-
-            IconButton(onClick = { passwordVisibility = !passwordVisibility }) {
-                Icon(imageVector = image, contentDescription = "show password")
-            }
-        },
-        visualTransformation = if (passwordVisibility) {
-            VisualTransformation.None
-        } else {
-            PasswordVisualTransformation('\u002A')
-        }
-    )
+        transformation = PasswordVisualTransformation('\u002A')
+    ) {
+        onPassChange(it)
+    }
 }
 
 @Preview
